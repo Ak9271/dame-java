ca6be6143ff9e71736fc4316f7c9499d94d1177a
nakchaya <akchayan.nihalananth@ynov.com>
2025-01-24 17:03:55 +0100

    içnt graphique initialiser fenetre et afficher

--- InterfaceGraphique.java
+++ InterfaceGraphique.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.time.format.DateTimeFormatter;
 import javax.swing.*;
 
 public class InterfaceGraphique {
@@ -14,5 +15,30 @@ public class InterfaceGraphique {
         this.jeu = jeu;
         this.joueurActif = jeu.getJoueurActif();
         initialiserFenetre();
+    }
+
+    private void initialiserFenetre() {
+        JFrame fond_blanc = new JFrame("");
+        fond_blanc.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        fond_blanc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        fenetre = new JFrame("Jeu de dames");
+        fenetre.setSize(600, 600);
+        JPanel damier = new JPanel(new GridLayout(8, 8));
+        mettreAJourPlateau();
+        fenetre.add(damier);
+        afficher();
+        fond_blanc.setVisible(true);
+    }
+
+    private void afficher () {
+        fenetre.setVisible(true);
+    }
+
+    public void mettreAJourPlateau() {
+        damier.removeAll();
+        for (int i = 0; i < 8; i++) {
+            
+        }
     }
 }
