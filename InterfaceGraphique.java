20dd5d9f99a0a17fbcfbf6c06531a0dbc651bab5
nakchaya <akchayan.nihalananth@ynov.com>
2025-01-24 17:45:30 +0100

    fonciton mettre a jour plateau

--- InterfaceGraphique.java
+++ InterfaceGraphique.java
@@ -21,11 +21,11 @@ public class InterfaceGraphique {
         JFrame fond_blanc = new JFrame("");
         fond_blanc.setExtendedState(JFrame.MAXIMIZED_BOTH);
         fond_blanc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
         fenetre = new JFrame("Jeu de dames");
         fenetre.setSize(600, 600);
         JPanel damier = new JPanel(new GridLayout(8, 8));
         mettreAJourPlateau();
+        fond_blanc.add(fenetre);
         fenetre.add(damier);
         afficher();
         fond_blanc.setVisible(true);
@@ -38,7 +38,15 @@ public class InterfaceGraphique {
     public void mettreAJourPlateau() {
         damier.removeAll();
         for (int i = 0; i < 8; i++) {
-            
+            for (int j = 0; j < 8; j++) {
+                Case caseActuelle = plateau.getCase(i, j);
+                CaseBouton caseBouton = new CaseBouton(caseActuelle.getPiece());
+                caseBouton.setBackground((i+j)%2 == 0 ? Color.WHITE : Color.BLACK);
+                actionClic(caseBouton, caseActuelle);
+                damier.add(caseBouton);
+            }
         }
+        damier.revalidate();
+        damier.repaint();
     }
 }
